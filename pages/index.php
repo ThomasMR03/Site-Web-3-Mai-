3834f7c67e74ea43a187b01df942ead7292b6f68
Thomas <Thomas.Face03@gmail.com>
2017-05-03 11:11:26 +0200

    Page d'accueil

--- pages/index.php
+++ pages/index.php
@@ -1,1 +1,26 @@
-<h2>Site Web</h2>
+<h2>Bonjour [<span style="color: red"><?= $_SESSION['Auth']?></span>] Je vous souhaite la bienvenue sur mon site web.</h2>
+
+
+<table class="table">
+		<thead>
+			<tr>
+				<td class="description">Que voulez-vous faire ?</td>
+				<td></td>
+			</tr>
+		</thead>
+		<tbody>
+			<tr>
+			<td><p class="action">Consulter les nouveaux articles ?</p></td>
+			<td style="padding: 30px; border: none;">
+			<a href="admin.php?p=posts.add" id="buttonAction">Let's go !</a>
+			</td>
+			</tr>
+
+			<tr>
+				<td><p class="action">Consulter la liste des utilisateurs ?</p></td>
+				<td style="padding: 30px; border: none;">
+					<a href="admin.php?p=users.index" id="buttonAction">Let's go !</a>
+				</td>
+			</tr>
+		</tbody>
+	</table>
--- readme.md
+++ readme.md
@@ -1,9 +1,13 @@
 Cahier des charges :
 
 1/ Méthode de travail : MVC
+
+
 2/ Fonctionnalité de départ : - Connexion/Déconnexion
-							  - Page Accueil
+                              - Page Accueil
                               - Zone Admin
+
+
 3/ Ajout De Fonctionnalité :
 Inscription :
 - MDP Sécuriser
